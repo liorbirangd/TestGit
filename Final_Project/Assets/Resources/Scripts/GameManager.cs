f243ac81d396e989f6c5ccc86705557dee4f14b9
lior1204 <lior12042000@gmail.com>
2022-01-27 12:59:17 +0200

    replace

---
+++ Final_Project/Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] public LayerMask platformLayer;
+
+    //object references
+    private Player player;
+
+    //game state variables
+    private bool isOverworldActive = true;
+
+    //input variables
+    private float moveInput = 0;
+    private bool jumpInput = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LocateGameObjects();
+    }
+    private void LocateGameObjects()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        CurrentInput();
+        if(isOverworldActive) PlayerMovement();
+    }
+    private void CurrentInput()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
+    }
+    private void PlayerMovement()
+    {
+        player.MoveX(moveInput);
+        if (jumpInput)
+        {
+            Debug.Log("jump");
+            player.Jump();
+        }
+    }
+    private void ManageExceptions(Exception e)
+    {
+
+    }
+}
---
+++ Final_Project/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private float jumpSpeed = 5;
+    [SerializeField] private int airJumpNum = 1;
+    private float groundExtraHeight = 0.1f;
+    //object references
+    private GameManager manager;
+    //component references
+    private Rigidbody2D _rigidBody;
+    private BoxCollider2D _collider;
+    //object state veriables
+    private bool isFacingRight;
+    private int airJumpCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LocateGameObjects();
+    }
+    private void InitializeStateVariables()
+    {
+        isFacingRight = true;
+        airJumpCount = airJumpNum;
+    }
+    private void LocateGameObjects()
+    {
+        manager = FindObjectOfType<GameManager>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGrounded())
+        {
+            airJumpCount = airJumpNum;
+        }
+    }
+    public void MoveX(float direction)
+    {
+        if ((isFacingRight && direction > 0) || (!isFacingRight && direction < 0))
+        {
+            MirrorAnimation();
+        }
+        _rigidBody.velocity=new Vector2(moveSpeed*direction,_rigidBody.velocity.y);
+    }
+    public void Jump()
+    {
+        if (IsGrounded())
+        {
+            Debug.Log("Player.Jump()");
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x,jumpSpeed);
+        }
+        else if(airJumpCount>0)
+        {
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, jumpSpeed);
+            airJumpCount -= 1;
+        }
+    }
+    private void MirrorAnimation()
+    {
+        isFacingRight = !isFacingRight;
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D ray= Physics2D.Raycast(_collider.bounds.center, Vector2.down,_collider.bounds.extents.y+groundExtraHeight,manager.platformLayer);
+        return ray.collider!=null;
+    }
+}
